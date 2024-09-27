53de5fd5c6d051ad39731663dbf67d13f13644a1
Caio Lima <caiojlimah@gmail.com>
2024-09-27 20:08:23 -0300

    [FEAT]: Clockfy cronjob

---
+++ index.js
@@ -0,0 +1,24 @@
+const { default: axios } = require('axios')
+const express = require('express')
+const cron = require('node-cron')
+require('dotenv').config()
+
+const app = express()
+
+const startClockfyWatch = async () => {
+  const requestPayload = {
+    "billable": false,
+    "description": "",
+    "projectId": null,
+    "taskId": null,
+    "tagIds": null,
+    "customFields": [],
+    "type": "REGULAR",
+    "continueStrategy": "REQUIRE_STOPPED"
+  }
+  await axios.post(process.env.CLOCKFY_URL, requestPayload, { headers: { "X-Auth-Token": process.env.X_AUTH_TOKEN } })
+
+}
+cron.schedule('0 9 * * *', startClockfyWatch)
+
+app.listen(3000, () => console.log('Ouvindo na porta 3000'))
